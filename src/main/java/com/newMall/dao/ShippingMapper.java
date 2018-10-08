85b3e336d2ea53cc9c096f383bb31132d3672cdf
root <qazwsxedc219@qq.com>
2018-10-08 23:29:35 +0800

    project init

---
+++ src/main/java/com/newMall/dao/ShippingMapper.java
@@ -0,0 +1,17 @@
+package com.newMall.dao;
+
+import com.newMall.pojo.Shipping;
+
+public interface ShippingMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(Shipping record);
+
+    int insertSelective(Shipping record);
+
+    Shipping selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(Shipping record);
+
+    int updateByPrimaryKey(Shipping record);
+}
